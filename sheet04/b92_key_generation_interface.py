50df0e42fc596db9973089a892df5829ba4c131f
Jurek Bleischwitz jurek <jurek@sdu-142554.c.sdu.dk>
2026-02-26 15:49:37 +0100

    instructions added

--- sheet04/b92_key_generation_interface.py
+++ sheet04/b92_key_generation_interface.py
@@ -4,6 +4,12 @@ from qiskit_aer import AerSimulator
 import random
 
 
+# In this task, the instructions can be found in the docstring of each function.
+# Follow the protocol from exercise sheet 4, the name of the variables is this same.
+# Do not rename the variables or change the data type of the output.
+# No additional imports or packages are necessary.
+
+
 def random_bitstring(length: int) -> List[int]:
     """
     Generate a random binary string of length `length`.
@@ -14,9 +20,9 @@ def random_bitstring(length: int) -> List[int]:
 
 def alice_qubit_b92(a: List[int]) -> QuantumCircuit:
     """
-    Prepare Alice's qubits
+    Prepare Alice's qubits.
     :param a: Alice's random bits
-    :return: Alice's qubits according to B92
+    :return: Alice's qubits according to B92 as qiskit QuantumCircuit
     """
 
 
@@ -24,7 +30,7 @@ def bob_measure_b92(qc: QuantumCircuit) -> List[List[int]]:
     """
     Prepare Bob's bits from Alice's qubits.
     :param qc: Alice's qubits
-    :return: A list (of lists of bits), one Bob's random bits, one entry is b according to B92 measurement.
+    :return: A list (of lists of bits), one entry Bob's random bits, one entry is b according to B92 measurement.
     """
 
 
